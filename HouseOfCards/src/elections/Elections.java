4e940d9fe7f8774775b1dc5a664a3786d39313ba
julietafrade97 <up201506530@fe.up.pt>
2018-11-01 11:34:26 +0000

    Listagem de estados e beliefs

--- HouseOfCards/src/elections/Elections.java
+++ HouseOfCards/src/elections/Elections.java
@@ -1,4 +1,5 @@
 package elections;
+
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
@@ -14,97 +15,130 @@ import jade.domain.FIPAAgentManagement.*;
 import jade.domain.FIPAException;
 
 public class Elections {
-	
-	private int minPopulation, maxPopulation, nrStates, nrCandidates;
+
+	private int minPopulation, maxPopulation, nrCandidates;
 	private Runtime rt;
 	private Profile p;
 	private ContainerController cc;
-	private ArrayList<String> States = new ArrayList<String>();
-	private ArrayList<String> Beliefs = new ArrayList<String>();
+	private ArrayList<String> states = new ArrayList<String>();
+	private ArrayList<String> beliefs = new ArrayList<String>();
 
-	public Elections(int minPopulation, int maxPopulation, int nrStates, int nrCandidates) throws StaleProxyException {
-		
+	public Elections(int minPopulation, int maxPopulation, int nrCandidates) throws StaleProxyException {
+
 		this.minPopulation = minPopulation;
 		this.maxPopulation = maxPopulation;
-		this.nrStates = nrStates;
 		this.nrCandidates = nrCandidates;
+
+		this.states.add("Alaska");
+		this.states.add("California");
+		this.states.add("Florida");
+		this.states.add("Hawaii");
+		this.states.add("Kansas");
+		//this.states.add("Montana");
+		//this.states.add("New Jersey");
+		//this.states.add("New York");
+		//this.states.add("Washington");
+		//this.states.add("Texas");
 		
+		this.beliefs.add("Liberalism");
+		this.beliefs.add("Conservatism");
+		this.beliefs.add("Communism");
+		this.beliefs.add("Socialism");
+		this.beliefs.add("Anarchism");
+		//this.beliefs.add("Nationalism");
+		//this.beliefs.add("Fascism");
+		//this.beliefs.add("Monarchism");
+		//this.beliefs.add("Republicanism");
+		//this.beliefs.add("Environmentalism");
+
 		this.rt = Runtime.instance();
 		this.p = new ProfileImpl(true);
-		this.cc =rt.createMainContainer(p);
-		
-		try{
+		this.cc = rt.createMainContainer(p);
+
+		try {
 			createVotersPerState();
 			createCandidates();
 			createChiefsOfStaff();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	
+
 	}
-	
-	public int getMinPopulation(){
+
+	public int getMinPopulation() {
 		return this.minPopulation;
 	}
-	
-	public int getMaxPopulation(){
+
+	public int getMaxPopulation() {
 		return this.maxPopulation;
 	}
 	
-	public int getNrStates(){
-		return this.nrStates;
+	public ArrayList<String> getStates() {
+		return states;
+	}
+
+	public void setStates(ArrayList<String> states) {
+		this.states = states;
 	}
 	
-	//min_state_population, max_state_population, nr_states, nr_candidates
+	public ArrayList<String> getBeliefs() {
+		return beliefs;
+	}
+
+	public void setBeliefs(ArrayList<String> beliefs) {
+		this.beliefs = beliefs;
+	}
+
+	// min_state_population, max_state_population, nr_candidates
 	public static void main(String args[]) throws StaleProxyException {
-		Elections elections = new Elections(Integer.parseInt(args[0]), Integer.parseInt(args[1]),Integer.parseInt(args[2]),Integer.parseInt(args[3]));
+		Elections elections = new Elections(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
 	}
-	
-	public void createVotersPerState() throws StaleProxyException{
-		
-		for(int id_state=0; id_state < this.nrStates; id_state++){
+
+	public void createVotersPerState() throws StaleProxyException {
+
+		for (int id_state = 0; id_state < this.states.size(); id_state++) {
 			int id_voterstate = 0;
-			
+
 			Random rnd = new Random();
-			int population = rnd.nextInt(this.maxPopulation+1 - this.minPopulation) + this.minPopulation;
-			System.out.println(" > STATE " + id_state + " POPULATION:" + population);
-			
-			while(id_voterstate < population ){
-				try{
-					String id = "voter_"+Integer.toString(id_state)+"_"+Integer.toString(id_voterstate);
-					AgentController ac = this.cc.createNewAgent(id,"agents.Voter",null);
+			int population = rnd.nextInt(this.maxPopulation + 1 - this.minPopulation) + this.minPopulation;
+			System.out.println(" > STATE " + this.states.get(id_state) + " " + " POPULATION: " + population);
+
+			while (id_voterstate < population) {
+				try {
+					String id = "voter_" + this.states.get(id_state) + "_" + Integer.toString(id_voterstate);
+					AgentController ac = this.cc.createNewAgent(id, "agents.Voter", null);
 					ac.start();
 					id_voterstate++;
-				}catch(Exception e){
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
+
 	}
-	
-	public void createCandidates() throws StaleProxyException{
-		
+
+	public void createCandidates() throws StaleProxyException {
+
 		System.out.println(" > CANDIDATES: " + this.nrCandidates);
-		
-		for(int id_candidate=0; id_candidate<this.nrCandidates; id_candidate++){
-			String id = "candidate_"+Integer.toString(id_candidate);
-			AgentController ac = this.cc.createNewAgent(id,"agents.Candidate",null);
+
+		for (int id_candidate = 0; id_candidate < this.nrCandidates; id_candidate++) {
+			String id = "candidate_" + Integer.toString(id_candidate);
+			AgentController ac = this.cc.createNewAgent(id, "agents.Candidate", null);
 			ac.start();
 		}
-			
+
 	}
-	
-	public void createChiefsOfStaff() throws StaleProxyException{
-		
-		System.out.println(" > CHIEFS OF STAFF: " + this.nrStates);
-		
-		for(int id_chief=0; id_chief<this.nrStates; id_chief++){
-			String id = "chiefofstaff_"+Integer.toString(id_chief);
-			AgentController ac = this.cc.createNewAgent(id,"agents.ChiefOfStaff",null);
+
+	public void createChiefsOfStaff() throws StaleProxyException {
+
+		System.out.println(" > CHIEFS OF STAFF: " + this.states.size());
+
+		for (int id_chief = 0; id_chief < this.states.size(); id_chief++) {
+			String id = "chiefofstaff_" + Integer.toString(id_chief);
+			AgentController ac = this.cc.createNewAgent(id, "agents.ChiefOfStaff", null);
 			ac.start();
 		}
-			
+
 	}
 
 }
